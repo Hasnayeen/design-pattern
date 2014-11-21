2e8531746a3adff81ef39a89763ebe8425a1bc1a
Hasnayeen <searching.nehal@gmail.com>
2014-11-21 20:09:06 +0600

    problem updated

--- adapter/problem.php
+++ adapter/problem.php
@@ -3,15 +3,15 @@ class Person{
 	private $name;
 	private $profession;
 
-	public function __construct($name){
+	public function __construct($name,$profession){
 		$this->name=$name;
+		$this->profession=$profession;
 	}
 
-	public function getProfession($profession){
-		$this->profession=$profession;
-		return $profession;
+	public function getProfession(){
+		return $this->name . " is a " . $this->profession;
 	}
 }
 
-$person = new Person("nehal");
-echo $person->getProfession("Doctor");
+$person = new Person("nehal","Doctor");
+echo $person->getProfession();
